34461f3b3afeaced7db0ff372b22562afa1ab734
Yann N'DA <yannnda97@gmail.com>
2026-02-24 14:45:41 +0100

    feature-join/leave--room

--- exo2/server/src/index.ts
+++ exo2/server/src/index.ts
@@ -4,6 +4,7 @@ import type { ServerMessage, ClientMessage } from '@shared/types';
 const server = new WebSocketServer({ port: 8686 });
 
 const nicks = new Map<WebSocket, string>();
+const rooms = new Map<string, Set<WebSocket>>();
 
 function broadcast(msg: ServerMessage) {
     for (const client of server.clients) {
@@ -15,8 +16,26 @@ function broadcastUserList() {
     const users = Array.from(nicks.values());
     broadcast({ type: 'user-list', users });
 }
+function joinRoom(socket: WebSocket, roomId: string) {
+    if (!rooms.has(roomId)) {
+        rooms.set(roomId, new Set());
+    }
+    const r = rooms.get(roomId)!;
+    if (r.has(socket)) {
+        return;
+    }
+    r.add(socket);
+}
 
-server.on('connection', (socket: WebSocket) => {
+function leaveRoom(socket: WebSocket, roomId: string) {
+    const r = rooms.get(roomId)!;
+    r.delete(socket);
+    if (rooms.get(roomId)!.size === 0) {
+        rooms.delete(roomId);
+    }
+}
+
+server.on('connection', (socket: WebSocket, ) => {
     const defaultNick = 'anon_' + Math.random().toString(36).slice(2, 6);
     nicks.set(socket, defaultNick);
 
@@ -42,6 +61,24 @@ server.on('connection', (socket: WebSocket) => {
             case 'typing': {
                 const nick = nicks.get(socket)!;
                 broadcast({ type: 'typing', nick });
+                break;
+            }
+            case 'join-room': {
+                if (rooms.get(msg.roomID)?.has(socket)) {
+                    broadcast({ type: 'system', text: `${nicks.get(socket)} is already in room ${msg.roomID}` });
+                    break;
+                }
+                joinRoom(socket, msg.roomID);
+                broadcast({ type: 'system', text: `${nicks.get(socket)} joined room ${msg.roomID}` });
+                break;
+            }
+            case 'leave-room': {
+                if (!rooms.get(msg.roomID)?.has(socket)) {
+                    broadcast({ type: 'system', text: `${nicks.get(socket)} is not in room ${msg.roomID}` });
+                    break;
+                }
+                leaveRoom(socket, msg.roomID);
+                broadcast({ type: 'system', text: `${nicks.get(socket)} left room ${msg.roomID}` });
                 break;
             }
         }
--- exo2/shared/types.ts
+++ exo2/shared/types.ts
@@ -2,9 +2,13 @@ export type ClientMessage =
   | { type: 'set-nick';  nick: string }
   | { type: 'chat';  text: string }
   | { type: 'typing';}
+  | { type: 'join-room'; roomID: string }
+  | { type: 'leave-room'; roomID: string }
 
 export type ServerMessage =
   | { type: 'chat';      nick: string; text: string; ts: number }
   | { type: 'system';    text: string }
   | { type: 'user-list'; users: string[] }
   | { type: 'typing';    nick: string }
+  | { type: 'join-room'; roomID: string }
+  | { type: 'leave-room'; roomID: string }
